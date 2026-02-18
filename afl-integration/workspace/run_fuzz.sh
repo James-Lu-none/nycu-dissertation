df2543d10be74b82475b50cb8c7940d8be347fa1
James-Lu-none <jamesabcde277@gmail.com>
2026-02-18 21:26:26 +0800

    refactor run_fuzz.sh to add CMPLOG window and adjust slave count

--- afl-integration/workspace/run_fuzz.sh
+++ afl-integration/workspace/run_fuzz.sh
@@ -16,8 +16,13 @@ tmux new-window -t $SESSION -n "asan" "afl-fuzz -i $INPUT_DIR -o $OUTPUT_DIR -S 
 tmux new-window -t $SESSION -n "msan" "afl-fuzz -i $INPUT_DIR -o $OUTPUT_DIR -S slave_msan -- ./target_msan @@"
 tmux new-window -t $SESSION -n "ubsan" "afl-fuzz -i $INPUT_DIR -o $OUTPUT_DIR -S slave_ubsan -- ./target_ubsan @@"
 
-# 3. 建立其餘 6 個普通 Slaves 視窗
-for i in {1..6}
+
+# 3. 建立 CMPLOG 視窗
+# https://github.com/AFLplusplus/AFLplusplus/blob/stable/instrumentation/README.cmplog.md
+tmux new-window -t $SESSION -n "cmplog" "afl-fuzz -i $INPUT_DIR -o $OUTPUT_DIR -S slave_cmplog -c ./target_cmplog -m none -- ./target_normal @@"
+
+# 4. 建立其餘 5 個普通 Slaves 視窗
+for i in {1..5}
 do
     tmux new-window -t $SESSION -n "slave_$i" "afl-fuzz -i $INPUT_DIR -o $OUTPUT_DIR -S slave_normal_$i -- ./target_normal @@"
 done
