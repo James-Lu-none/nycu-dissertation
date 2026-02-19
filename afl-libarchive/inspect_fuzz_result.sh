e93590d9437771cca975c3d973a9f06524f80220
James-Lu-none <jamesabcde277@gmail.com>
2026-02-19 15:04:31 +0800

    add inspect_fuzz_result.sh script for coverage data collection and report generation

---
+++ afl-libarchive/inspect_fuzz_result.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# clang++ --coverage -o target_gcov target.c
+
+# afl-cov -d ./out --live --code-dir ./src_code/ -c "./target_gcov @@" --all-tests
+clang -fprofile-instr-generate -fcoverage-mapping -O0 target.c -o target_cov
+
+mkdir -p profraws
+export LLVM_PROFILE_FILE="profraws/code-%p.profraw"
+
+for file in /workspace/out/*/queue/id*; do
+    LLVM_PROFILE_FILE="/workspace/profraws/code-%m-%p.profraw" /workspace/target_cov "$file"
+done
+
+# 3. 合併數據
+echo "正在合併數據..."
+llvm-profdata-19 merge -sparse profraws/*.profraw -o merged.profdata
+
+# 4. 生成 HTML 報表
+echo "正在產出報表..."
+llvm-cov-19 show ./target_cov -instr-profile=merged.profdata -format=html -output-dir=cov_report
+
+echo "完成！請在瀏覽器打開 ./cov_report/index.html"
