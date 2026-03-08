96acdfafa9ca9fa1f557ccc3030dd2a1dc5c0276
James-Lu-none <jamesabcde277@gmail.com>
2026-03-08 13:18:18 +0800

    update run_all.sh

--- tests/projects/libxml2/run_all.sh
+++ tests/projects/libxml2/run_all.sh
@@ -3,14 +3,14 @@
 SESSION="fuzz_session"
 
 INPUT_DIR="./in"    
-OUTPUT_DIR="./out"      
+OUTPUT_DIR="./artifacts/out"      
 
 tmux kill-session -t $SESSION 2>/dev/null
 rm -rf $OUTPUT_DIR
-rm cfg_edges.txt
-mkdir -p $INPUT_DIR
+# rm cfg_edges.txt
+# mkdir -p $INPUT_DIR
 mkdir -p $OUTPUT_DIR
-echo "init_seed" > $INPUT_DIR/seed.txt
+# echo "init_seed" > $INPUT_DIR/seed.txt
 
 afl-clang-lto target.c -o target_normal
 
