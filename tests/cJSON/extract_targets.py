473d73e67f11a6af01cde8b75dd743858cb593ec
James-Lu-none <jamesabcde277@gmail.com>
2026-03-31 17:12:50 +0800

    refactor: update Dockerfile to use absolute path for extract_targets.py

--- tests/cJSON/extract_targets.py
+++
@@ -1,9 +0,0 @@
-import csv
-with open("security_results.csv", "r") as f:
-    reader = csv.reader(f)
-    with open("targets.csv", "w") as out:
-        for row in reader:
-            if len(row) >= 6:
-                path = row[4].lstrip("/")
-                line = row[5]
-                out.write(f"{path},{line}\n")
