5ec6940bd0e80e225f7832fc0051461dd94c8aca
James-Lu-none <jamesabcde277@gmail.com>
2026-05-28 20:17:56 +0800

    add targets

--- bench/ICD/swftophp-4.8/script.sh
+++
@@ -1,4 +0,0 @@
-tmux new-session -d -s swftophp -n "main" "afl-fuzz -i in -o out -M main -- ./swftophp @@"
-
-# echo "Fuzzing session 'motivating-example' started!"
-# echo "Use 'tmux attach -t motivating-example' to see progress."
