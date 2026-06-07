105e5b488f208491d3f8f3f0b5b36f7a0e0c735f
James-Lu-none <jamesabcde277@gmail.com>
2026-06-07 14:50:25 +0800

    update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
 trial=(1 2 3 4 5)
-CVE="CVE-2018-20427"
+CVE="binutils-2.26_cxxfilt_CVE-2016-4492"
 root="./artifact/${CVE}"
 mkdir -p ${root}
 
-baseline="swftophp-afl-origin"
-icd="swftophp-afl-icd"
+baseline="afl-origin"
+icd="afl-icd"
 
 for i in ${trial[@]}; do
   
     mkdir -p ${root}/${baseline}/trial${i}
     mkdir -p ${root}/${icd}/trial${i}
 
-    docker cp ${baseline}-${i}:/workspace/dgf_blocks_hit.txt ${root}/${baseline}/trial${i}/
-    docker cp ${baseline}-${i}:/workspace/dgf_target_reached.txt ${root}/${baseline}/trial${i}/
-    docker cp ${baseline}-${i}:/workspace/dgf_block_mapping.txt ${root}/${baseline}/trial${i}/
-    docker cp ${baseline}-${i}:/workspace/dgf_compile_info.txt ${root}/${baseline}/trial${i}/
-    docker cp ${baseline}-${i}:/workspace/out ${root}/${baseline}/trial${i}/
+    docker cp ${CVE}-${baseline}-${i}:/workspace/dgf_blocks_hit.txt ${root}/${baseline}/trial${i}/
+    docker cp ${CVE}-${baseline}-${i}:/workspace/dgf_target_reached.txt ${root}/${baseline}/trial${i}/
+    docker cp ${CVE}-${baseline}-${i}:/workspace/dgf_block_mapping.txt ${root}/${baseline}/trial${i}/
+    docker cp ${CVE}-${baseline}-${i}:/workspace/dgf_compile_info.txt ${root}/${baseline}/trial${i}/
+    docker cp ${CVE}-${baseline}-${i}:/workspace/out ${root}/${baseline}/trial${i}/
 
-    docker cp ${icd}-${i}:/workspace/dgf_blocks_hit.txt ${root}/${icd}/trial${i}/
-    docker cp ${icd}-${i}:/workspace/dgf_target_reached.txt ${root}/${icd}/trial${i}/
-    docker cp ${icd}-${i}:/workspace/dgf_block_mapping.txt ${root}/${icd}/trial${i}/
-    docker cp ${icd}-${i}:/workspace/dgf_compile_info.txt ${root}/${icd}/trial${i}/
-    docker cp ${icd}-${i}:/workspace/out ${root}/${icd}/trial${i}/
+    docker cp ${CVE}-${icd}-${i}:/workspace/dgf_blocks_hit.txt ${root}/${icd}/trial${i}/
+    docker cp ${CVE}-${icd}-${i}:/workspace/dgf_target_reached.txt ${root}/${icd}/trial${i}/
+    docker cp ${CVE}-${icd}-${i}:/workspace/dgf_block_mapping.txt ${root}/${icd}/trial${i}/
+    docker cp ${CVE}-${icd}-${i}:/workspace/dgf_compile_info.txt ${root}/${icd}/trial${i}/
+    docker cp ${CVE}-${icd}-${i}:/workspace/out ${root}/${icd}/trial${i}/
 done
 
 chown -R $(id -u):$(id -g) ${root}
